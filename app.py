af4300d1bee43b70c3ebb4d8868e5bbbe7171d1a
rarepepi <pepimartinez@protonmail.com>
2022-05-08 03:44:14 -0400

    ok

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 from Crypto.Hash import keccak
-
+from time import time
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
